975a345274a3a7f206c1c716a3bb56179ccbc3fe
emrahbeydilli <emrahbeydilli@gmail.com>
2022-04-02 01:59:57 +0300

    uploading data set

---
+++ script.js
@@ -0,0 +1,32 @@
+var database = [
+	{
+		username: "furkan",
+		password: "webmaster",
+		friends: ["abdullah", "yeşim"]
+	},
+	{
+		username: "abdullah",
+		password: "123",
+		friends: ["furkan", "yeşim"]
+	},
+	{
+		username: "yeşim",
+		password: "777",
+		friends: ["abdullah", "furkan"]
+	}
+];
+var newsFeed = [
+	{
+		username: "abdullah",
+		tweet: "Bilet fiyatları uçtu uçtu!!!"
+	},
+	{
+		username: "yeşim",
+		tweet: "şarjım bitti :("
+	}
+	,
+	{
+		username: "furkan",
+		tweet: "Javascript bir harika"
+	}
+];
